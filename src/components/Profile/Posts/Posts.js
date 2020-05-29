9fd3d23e49f0e4725d52b8be04896755f610822d
brbr aww <legendary.grassman@gmail.com>
2020-05-29 14:46:46 +0300

    87. Оптимизация функциональной компоненты Posts с помощью React.memo

--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -5,24 +5,25 @@ import {Field, reduxForm, reset} from "redux-form";
 import {maxLenghtCreator, requiredField} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 
-const Posts = (props) => {
-    let PostsItems = props.PostsData.map( (p) => <Post id={p.id} text={ p.text } likesCount={p.likesCount} /> );
+const Posts = React.memo(props => {
+    let PostsItems = props.PostsData.map((p) => <Post id={p.id} text={p.text} likesCount={p.likesCount}/>);
 
     let addNewPost = (values) => {
         props.addPost(values.newPostText);
     }
 
     return (
-        <div className={ classes.all }>
+        <div className={classes.all}>
 
-            <AddPostFormRedux onSubmit={addNewPost} />
-            <div className={ classes.posts }>
+            <AddPostFormRedux onSubmit={addNewPost}/>
+            <div className={classes.posts}>
                 {PostsItems}
             </div>
 
         </div>
     );
-}
+});
+
 const maxLenght10 = maxLenghtCreator(10)
 const AddNewPostForm = (props) => {
 
@@ -39,24 +40,7 @@ const AddNewPostForm = (props) => {
         </form>
     )
 }
-/*class AddNewPostForm extends React.Component {
-    render() {
-        return (
-            <form onSubmit={this.props.handleSubmit} className={classes.addPost}>
-                <div>
-                    <Field
-                        component={'textarea'}
-                        name={'newPostText'}
-                        placeholder='Enter new post'
-                    />
-                </div>
-                <div>
-                    <button>Add post</button>
-                </div>
-            </form>
-        )
-    }
-}*/
+
 const afterSubmit = (result, dispatch) =>
     dispatch(reset('postsAddNewPostForm'));
 const AddPostFormRedux = reduxForm({form:'postsAddNewPostForm', onSubmitSuccess: afterSubmit})(AddNewPostForm)
